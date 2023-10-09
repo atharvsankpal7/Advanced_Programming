d6b19486fcba0c9d2dc2037a0ba733ffbd196942
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2023-10-08 22:26:27 -0700

    Optimization

--- Experiment_02/06.py
+++ Experiment_02/06.py
@@ -6,14 +6,17 @@ def password_validation(input_string):
     has_lower_character = False
     has_number_character = False
     for i in input_string:
-        if i.isdigit():
-            has_number_character = True
-        if i.islower():
-            has_lower_character = True
-        if i.isupper():
-            has_capital_character = True
-    if(has_capital_character and has_lower_character and has_number_character):
-        return True
+        if(not has_number_character):
+            if i.isdigit():
+                has_number_character = True
+        if(not has_lower_character):
+            if i.islower():
+                has_lower_character = True
+        if(not has_capital_character):
+            if i.isupper():
+                has_capital_character = True
+        if(has_capital_character and has_lower_character and has_number_character):
+            return True
     return False
     
 string = "Password123"
